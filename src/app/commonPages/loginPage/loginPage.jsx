cc388016fd04031b24bf6a3a3dc5fc20db5bee4f
Gowantha Charithal <gowantha.18@cse.mrt.ac.lk>
2021-09-07 16:21:28 +0530

    Created the Login UI, Added the logo

--- src/app/commonPages/loginPage/loginPage.jsx
+++ src/app/commonPages/loginPage/loginPage.jsx
@@ -1,5 +1,109 @@
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  makeStyles,
+  Typography,
+  Container,
+} from '@material-ui/core';
+import { useState } from 'react';
+import { ReactComponent as Logo1 } from '../../../Logo 6.2.svg';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(10),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  loginForm: {
+    width: '100%',
+    marginTop: theme.spacing(0),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  unilabsLogo: {
+    marginTop: theme.spacing(2),
+    width: '50%',
+  },
+}));
+
 function LoginPage() {
-  return <div>Login</div>;
+  const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="bigContainer">
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h4">
+            Welcome to UniLabs
+          </Typography>
+          <Logo1 className={classes.unilabsLogo} />
+          <div className={classes.loginForm}>
+            <Typography component="h1" variant="h5">
+              Log in
+            </Typography>
+            <form className={classes.form} noValidate onSubmit={handleLogin}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                autoFocus
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Log In
+              </Button>
+            </form>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default LoginPage;
