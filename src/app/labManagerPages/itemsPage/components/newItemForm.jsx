9006719c28f3182754f549c5e78c9a07cb148740
Avishka-Shamendra <avishka199849@gmail.com>
2021-09-08 10:16:30 +0530

    barcode download functionality added

--- src/app/labManagerPages/itemsPage/components/newItemForm.jsx
+++ src/app/labManagerPages/itemsPage/components/newItemForm.jsx
@@ -1,4 +1,13 @@
-import { makeStyles, Container, Box } from '@material-ui/core';
+import {
+  makeStyles,
+  Container,
+  Box,
+  Typography,
+  Button,
+} from '@material-ui/core';
+import { useRef, useState } from 'react';
+import Barcode from 'react-barcode';
+import { exportComponentAsJPEG } from 'react-component-export-image';
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -8,23 +17,66 @@ const useStyles = makeStyles(theme => ({
     cursor: 'pointer',
     maxWidth: 700,
   },
+  barcodeBox: {
+    padding: theme.spacing(2),
+  },
 }));
 
 function NewItemForm() {
   const classes = useStyles();
+  const barcodeRef = useRef('unilab-barcode');
+  const [barcodeState, setBarcodeState] = useState(false);
+  const handleBarcodeOpen = () => setBarcodeState(true);
+  const handleBarcodeClose = () => setBarcodeState(false);
 
   return (
     <Container>
-      <Box
-        border={3}
-        borderRadius={5}
-        borderColor="secondary.main"
-        className={classes.card}
-        fontSize="h5.fontSize"
-        align="center"
-      >
-        <div>Click to Generate Barcode and Add an Item</div>
-      </Box>
+      {barcodeState ? (
+        <Box
+          border={3}
+          align="center"
+          className={classes.barcodeBox}
+          borderRadius={5}
+          borderColor="secondary.main"
+        >
+          <Typography variant="h6">
+            Generated Barcode for The New Item
+          </Typography>
+          <Typography color="error" fontSize="">
+            <strong>
+              Download the Following Barcode and Paste on the Item
+            </strong>
+          </Typography>
+          <div ref={barcodeRef}>
+            <Barcode value="f35c02a3" fontSize={20} />
+          </div>
+          <Box m={1} />
+          <Button
+            onClick={() => {
+              exportComponentAsJPEG(barcodeRef);
+              handleBarcodeClose();
+            }}
+            variant="outlined"
+            color="secondary"
+          >
+            Download Barcode
+          </Button>
+        </Box>
+      ) : (
+        <>
+          <Box
+            border={3}
+            borderRadius={5}
+            borderColor="secondary.main"
+            className={classes.card}
+            fontSize="h5.fontSize"
+            align="center"
+            onClick={handleBarcodeOpen}
+          >
+            <div>Click to Generate Barcode and Add an Item</div>
+          </Box>
+        </>
+      )}
     </Container>
   );
 }
