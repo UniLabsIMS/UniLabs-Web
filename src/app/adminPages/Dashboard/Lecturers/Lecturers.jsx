007d77eb570a3e23046d9a3621c61c5ab86b9970
Gowantha Charithal <gowantha.18@cse.mrt.ac.lk>
2021-09-08 15:18:37 +0530

    created the lecturer page

--- src/app/adminPages/Dashboard/Lecturers/Lecturers.jsx
+++ src/app/adminPages/Dashboard/Lecturers/Lecturers.jsx
@@ -1,3 +1,118 @@
-export default function LecturerTable() {
-  return <div>Lecturer Table</div>;
+// export default function LecturerTable() {
+//   return <div>Lecturer Table</div>;
+// }
+
+import { makeStyles } from '@material-ui/core/styles';
+import { Button } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import RegisterLecturer from './components/lecturerRegistrationForm';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData(
+    'L0001',
+    'Lecturer_1',
+    'Department_1',
+    <Button variant="contained" color="secondary">
+      Delete
+    </Button>,
+  ),
+  createData(
+    'L0002',
+    'Lecturer_2',
+    'Department_2',
+    <Button variant="contained" color="secondary">
+      Delete
+    </Button>,
+  ),
+  createData(
+    'L0003',
+    'Lecturer_3',
+    'Department_1',
+    <Button variant="contained" color="secondary">
+      Delete
+    </Button>,
+  ),
+  createData(
+    'L0004',
+    'Lecturer_4',
+    'Department_4',
+    <Button variant="contained" color="secondary">
+      Delete
+    </Button>,
+  ),
+  createData(
+    'L0005',
+    'Lecturer_5',
+    'Department_3',
+    <Button variant="contained" color="secondary">
+      Delete
+    </Button>,
+  ),
+  createData(
+    'L0006',
+    'Lecturer_6',
+    'Department_3',
+    <Button variant="contained" color="secondary">
+      Delete
+    </Button>,
+  ),
+  createData(
+    'L0007',
+    'Lecturer_7',
+    'Department_1',
+    <Button variant="contained" color="secondary">
+      Delete
+    </Button>,
+  ),
+];
+
+export default function StudentTable() {
+  const classes = useStyles();
+
+  return (
+    <div className="largerContainer">
+      <TableContainer component={Paper}>
+        <RegisterLecturer />
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Name</TableCell>
+              <TableCell align="right">Department</TableCell>
+              <TableCell align="right" />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 }
---
+++ src/app/adminPages/Dashboard/Lecturers/components/lecturerRegistrationForm.jsx
@@ -0,0 +1,162 @@
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  makeStyles,
+  Typography,
+  Container,
+} from '@material-ui/core';
+import { useState } from 'react';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    marginTop: theme.spacing(5),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  loginForm: {
+    width: '100%',
+    marginTop: theme.spacing(0),
+    marginBottom: theme.spacing(3),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 0),
+    display: 'flex',
+    width: '40%',
+    marginRight: theme.spacing(5),
+    marginLeft: theme.spacing(5),
+  },
+  unilabsLogo: {
+    marginTop: theme.spacing(2),
+    width: '50%',
+  },
+  formControl: {
+    marginTop: theme.spacing(2),
+    marginRight: theme.spacing(5),
+    marginLeft: theme.spacing(5),
+    margin: theme.spacing(0),
+    width: '40%',
+  },
+  formLine: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  texts: {
+    display: 'flex',
+    width: '40%',
+    marginRight: theme.spacing(5),
+    marginLeft: theme.spacing(5),
+  },
+  buttons: {
+    width: '40%',
+  },
+}));
+
+function RegisterLecturer() {
+  const classes = useStyles();
+  const [email, setEmail] = useState('');
+  const [lecturerId, setLecturerId] = useState('');
+  const [department, setDepartment] = useState('');
+
+  const handleChange = event => {
+    setDepartment(event.target.value);
+  };
+
+  const handleLogin = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="bigContainer">
+      <Container component="main">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <div className={classes.loginForm}>
+            <div className={classes.formLine}>
+              <Typography component="h1" variant="h5">
+                Register a new Lecturer
+              </Typography>
+            </div>
+            <form className={classes.form} noValidate onSubmit={handleLogin}>
+              <div className={classes.formLine}>
+                <TextField
+                  className={classes.texts}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  autoFocus
+                />
+                <TextField
+                  className={classes.texts}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="lecturerId"
+                  label="Lecturer ID"
+                  name="lecturerId"
+                  autoComplete="lecturerId"
+                  value={lecturerId}
+                  onChange={e => setLecturerId(e.target.value)}
+                />
+              </div>
+              <div className={classes.formLine}>
+                <FormControl className={classes.formControl}>
+                  <InputLabel id="demo-simple-select-label">
+                    Department
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={department}
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={0}>CSE</MenuItem>
+                    <MenuItem value={1}>ENTC</MenuItem>
+                    <MenuItem value={2}>CE</MenuItem>
+                    <MenuItem value={3}>ME</MenuItem>
+                    <MenuItem value={4}>CPE</MenuItem>
+                  </Select>
+                </FormControl>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Register Lecturer
+                </Button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default RegisterLecturer;
