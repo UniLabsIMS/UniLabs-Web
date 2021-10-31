c932bfb1ed513c4067d23a6fda7fee347205d4e9
Shanmugabavan <shanmugabavan25621@gmail.com>
2021-10-31 11:11:47 +0530

    lecturerRequestReducer tests partially done

---
+++ src/tests/store/reducers/lecturer/lecturerRequestReducer.test.js
@@ -0,0 +1,76 @@
+import LecturerRequest from '../../../../models/lecturerRequest';
+import {
+  LECTURER_REQUEST_LOADING,
+  LECTURER_REQUEST_LOADED,
+  LECTURER_REQUEST_ERROR,
+} from '../../../../store/actionTypes/lecturerActionTypes';
+import lecturerRequestReducer from '../../../../store/reducers/lecturerReducers/lecturerRequestReducer';
+
+describe('Lecturer - Request Reducer', () => {
+  const initialState = {
+    request: null,
+    isRequestLoading: false,
+    isRequestError: false,
+    reloadRequest: false,
+  };
+
+  it('returns the initial state when an action type is not passed', () => {
+    const reducer = lecturerRequestReducer(undefined, {});
+
+    expect(reducer).toEqual(initialState);
+  });
+
+  it('handles LECTURER_REQUEST_LOADING STATE event as expected', () => {
+    const reducer = lecturerRequestReducer(initialState, {
+      type: LECTURER_REQUEST_LOADING,
+    });
+
+    expect(reducer).toEqual({
+      ...initialState,
+      isRequestLoading: true,
+      isRequestError: false,
+      reloadRequest: false,
+    });
+  });
+
+  //   it('handles LECTURER_REQUEST_LOADED_STATE event as expected', () => {
+  //     const testStartState = {
+  //       ...initialState,
+  //       isRequestLoading: true,
+  //       isRequestError: false,
+  //       reloadRequest: false,
+  //     };
+
+  //     const reducer = lecturerRequestReducer(testStartState, {
+  //       type: LECTURER_REQUEST_LOADED,
+  //       payload: [],
+  //     });
+
+  //     expect(reducer).toEqual({
+  //       ...testStartState,
+  //       isRequestLoading: false,
+  //       request: new LecturerRequest([]),
+  //       isRequestError: false,
+  //     });
+  //   });
+
+  it('handles LECTURER_REQUEST_ERROR event event as expected', () => {
+    const testStartState = {
+      ...initialState,
+      isRequestLoading: true,
+      isRequestError: false,
+      reloadRequest: false,
+    };
+
+    const reducer = lecturerRequestReducer(testStartState, {
+      type: LECTURER_REQUEST_ERROR,
+    });
+
+    expect(reducer).toEqual({
+      ...testStartState,
+      isRequestLoading: false,
+      request: null,
+      isRequestError: true,
+    });
+  });
+});
